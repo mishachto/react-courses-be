b7cacbeec1453a9f843a5e710bf9019f34fa1ca4
misha <solonyna.misha@gmail.com>
2021-11-04 15:39:51 +0200

    hot fix

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ require('dotenv').config()
 interface IConfigSettings {
     host: string;
     user: string;
-    port: string;
+    port: string | number;
     password: string;
     database: string;
     SECRET_KEY: string;
@@ -26,27 +26,27 @@ const config: IConfig = {
         SECRET_KEY: "asd23j34jf983jfoiqwej98d342q09gfqkw"
     },
     qa: {
-        host: "",
-        user: "",
-        password: "",
-        port: "",
-        database: "",
+        host: "ec2-54-73-110-26.eu-west-1.compute.amazonaws.com",
+        database: "d3p2etlvl7e8gr",
+        user:     'uhedcqwspchslt',
+        password: '5f2e1d6f25813353eb468405fe1c4c1f43f4fa8d984546ee6016d6efea95b651',
+        port: 5432,
         SECRET_KEY: "asd23j34jf983jfoiqwej98d342q09gfqkw"
     },
     uat: {
-        host: "",
-        user: "",
-        password: "",
-        port: "",
-        database: "",
+        host: "ec2-54-73-110-26.eu-west-1.compute.amazonaws.com",
+        database: "d3p2etlvl7e8gr",
+        user:     'uhedcqwspchslt',
+        password: '5f2e1d6f25813353eb468405fe1c4c1f43f4fa8d984546ee6016d6efea95b651',
+        port: 5432,
         SECRET_KEY: "asd23j34jf983jfoiqwej98d342q09gfqkw"
     },
     prod: {
-        host: "",
-        user: "",
-        password: "",
-        port: "",
-        database: "",
+        host: "ec2-54-73-110-26.eu-west-1.compute.amazonaws.com",
+        database: "d3p2etlvl7e8gr",
+        user:     'uhedcqwspchslt',
+        password: '5f2e1d6f25813353eb468405fe1c4c1f43f4fa8d984546ee6016d6efea95b651',
+        port: 5432,
         SECRET_KEY: "asd23j34jf983jfoiqwej98d342q09gfqkw"
     }
 }
--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,17 @@ module.exports = {
     
       client: 'pg',
       connection: {
-        host: "localhost",
+        // host: "localhost",
+        // port: 5432,
+        // database: 'todosBD',
+        // user:     'postgres',
+        // password: 'solonyna22'
+        
+        host: "ec2-54-73-110-26.eu-west-1.compute.amazonaws.com",
+        database: "d3p2etlvl7e8gr",
+        user:     'uhedcqwspchslt',
+        password: '5f2e1d6f25813353eb468405fe1c4c1f43f4fa8d984546ee6016d6efea95b651',
         port: 5432,
-        database: 'todosBD',
-        user:     'postgres',
-        password: 'solonyna22'
       },
       pool: {
         min: 2,
